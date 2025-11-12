1be824835ffa996c6b968cf3e03efc0a38778477
tanishaness <wordsofvoice2210@gmail.com>
2025-11-13 00:42:34 +0530

    made sections

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Section2 from './assets/components/section2/section2.jsx'
+import Section1 from './assets/components/section1/section1.jsx'
 
 const App = () => {
   return (
-    <div> 
-      
+    <div>
+      <Section1 />
+      <Section2 />
     </div>
   )
 }
---
+++ my-project/src/assets/components/section1/section1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <div className='h-screen w-full bg-red-900'></div>
+  )
+}
+
+export default Section1
